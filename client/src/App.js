08f82472ec9ed8d05b36d43fd20ec8290ec555ee
sid@747 <sidmm747@gmail.com>
2024-04-22 12:36:22 +0530

    MERN project Initialized

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
+
+const App = () => {
+  return (
+   <Container maxWidth="lg">
+      <AppBar position='static' color='inherit'>
+        
+      </AppBar>
+   </Container>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js'
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://Sid747:siddik2471@cluster0.xpkwjos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const PORT =process.env.PORT || 5000;
+
+// const CONNECTION_URL = process.env.CONNECTION_URL
+// const PORT =process.env.PORT;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is Running on port:${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+
+// mongoose.set('useFindAndModify',false);
