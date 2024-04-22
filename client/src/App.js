dd8bcbc9cb68db1617e64a828f20f54d21e7893e
sid@747 <sidmm747@gmail.com>
2024-04-22 17:00:16 +0530

    Material UI Library

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import memories from "./assets/memories.png"
+import Forms from './components/Forms/Forms';
+import Posts from './components/Posts/Posts';
+
+import useStyle from './style'
 const App = () => {
+  const classes=useStyle();
   return (
-   <Container maxWidth="lg">
-      <AppBar position='static' color='inherit'>
-        
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+        <img src={memories} alt='memories' height="60" />
       </AppBar>
-   </Container>
+
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+            <Grid item xs={12} sm={7}>
+               <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+             <Forms />
+            </Grid>
+          </Grid> 
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
---
+++ client/src/components/Posts/Post/style.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const style = () => {
+  return (
+   <h1>post</h1>
+  )
+}
+
+export default style
