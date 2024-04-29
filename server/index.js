2482d44827021b39102655c131850ffa0ac9ddd9
sid@747 <sidmm747@gmail.com>
2024-04-30 00:20:41 +0530

    added .env

--- server/index.js
+++ server/index.js
@@ -4,8 +4,9 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js'
-// import dotenv from 'dotenv';
-// dotenv.config();
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -16,11 +17,8 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://Sid747:siddik2471@cluster0.xpkwjos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-const PORT =process.env.PORT || 5000;
-
-// const CONNECTION_URL = process.env.CONNECTION_URL
-// const PORT =process.env.PORT;
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT =process.env.PORT;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server is Running on port:${PORT}`)))
